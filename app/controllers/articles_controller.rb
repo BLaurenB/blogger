6c2c54928d5982072b9ce73f25eae2871eac53b6
BLaurenB <blbillington1@gmail.com>
2017-10-14 16:43:24 -0600

    wip

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,22 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # fail #halts the request, allowing you to examine the request
+
+    # @article = Article.new
+    # @article.title = params[:article][:title] #because the hash is nested
+    # @article.body = params[:article][:body] #because the hash is nested
+    # @article.save
+
+    @article = Article.new(params[:article])
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
 end
